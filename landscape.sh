792df378ddf2357bd68b45a69e8afcde797a8220
dr <dustenryen@gmail.com>
2022-12-06 21:36:50 -0800

    landscape_portrait_split

---
+++ landscape.sh
@@ -0,0 +1,26 @@
+if ! command -v nvm &> /dev/null
+then
+    curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh -o install_nvm.sh
+    bash install_nvm.sh
+    rm install_nvm.sh
+    nvm install v14.18.0
+fi
+
+if  [ -d "venv" ] 
+then
+# nothing
+else
+    python3 -m venv venv
+fi
+
+. venv/bin/activate
+nvm use  v14.18.0
+
+if ! command -v squoosh-cli &> /dev/null
+then
+    npm i -g @squoosh/cli
+fi
+
+
+# run the script
+python3 main_landscape.py
---
+++ main_portrait.py
@@ -0,0 +1,77 @@
+import subprocess
+import os
+from pathlib import Path
+resize_settings = """
+{
+"enabled": true,
+"width": 1280,
+"height": 1920,
+"method": "lanczos3",
+"fitMethod": "stretch",
+"premultiply": true,
+"linearRGB": true
+}
+""".replace("\n", "")
+export_settings = """
+{
+"quality": 75,
+"target_size": 0,
+"target_PSNR": 0,
+"method": 4,
+"sns_strength": 50,
+"filter_strength": 60,
+"filter_sharpness": 0,
+"filter_type": 1,
+"partitions": 0,
+"segments": 4,
+"pass": 1,
+"show_compressed": 0,
+"preprocessing": 0,
+"autofilter": 0,
+"partition_limit": 0,
+"alpha_compression": 1,
+"alpha_filtering": 1,
+"alpha_quality": 100,
+"lossless": 0,
+"exact": 0,
+"image_hint": 0,
+"emulate_jpeg_size": 0,
+"thread_level": 0,
+"low_memory": 0,
+"near_lossless": 100,
+"use_delta_palette": 0,
+"use_sharp_yuv": 0
+}
+""".replace("\n", "")
+export_mode = "--webp"
+input_path = "./input_portrait"
+output_path = "./output"
+def setup():
+    Path(input_path).mkdir(exist_ok=True)
+    Path(output_path).mkdir(exist_ok=True)
+
+def run():
+    for file in os.listdir(input_path):
+        path = input_path + "/" + file
+        try: 
+            if file == ".DS_Store":
+                print(f"Skipping [{path}]")
+                continue
+            print(f"Exporting [{path}]")
+            export_asset(input_path,file)
+        except (RuntimeError, TypeError, NameError) as e:
+            print(f"Error exporting {path}: {e} --- Skipped")
+            continue
+
+
+def export_asset(path, file):
+    cmd = f"squoosh-cli --resize '{resize_settings}' {export_mode} '{export_settings}' -d {output_path} {path}/{file}"
+    subprocess.run(cmd, shell=True, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    output_file = os.path.splitext(file)[0] + "." + export_mode.removeprefix("--")
+    size_before = os.path.getsize(Path(path, file))
+    size_after = os.path.getsize(Path(output_path, output_file))
+    delta = size_before - size_after
+    print(f"Removed {'{:,}'.format(delta)} bytes ({'{:.1%}'.format( delta / size_before)} %)\n")
+    
+setup()
+run()
